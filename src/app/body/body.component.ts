be7ca1cd2840f8e66b2d84461b311b689e64f78c
maxmc <maxi_mc@live.com.ar>
2020-09-05 18:20:51 -0300

    bug button nav, color text, footer

--- src/app/body/body.component.ts
+++ src/app/body/body.component.ts
@@ -8,89 +8,9 @@ import { Breakpoints, BreakpointObserver } from '@angular/cdk/layout';
   styleUrls: ['./body.component.css'],
 })
 export class BodyComponent {
-  cards = this.breakpointObserver.observe(Breakpoints.Handset).pipe(
-    map(({ matches }) => {
-      if (matches) {
-        return [
-          {
-            title: 'Web',
-            cols: 1,
-            rows: 1,
-            src: '/assets/img/icons-web.png',
-          },
-          {
-            title: 'Sistemas',
-            cols: 1,
-            rows: 1,
-            src: '/assets/img/icons-sistemas.png',
-          },
-          {
-            title: 'Cloud',
-            cols: 1,
-            rows: 1,
-            src: '/assets/img/icons-cloud.png',
-          },
-          {
-            title: 'Consultoria',
-            cols: 1,
-            rows: 1,
-            src: '/assets/img/icons-consultoria.png',
-          },
-          {
-            title: 'Contacto',
-            cols: 1,
-            rows: 1,
-            src: '/assets/img/icons-contact.png',
-          },
-          {
-            title: 'Clientes',
-            cols: 1,
-            rows: 1,
-            src: '/assets/img/icons-clientes.png',
-          },
-        ];
-      }
+  public isCollapsed = true;
 
-      return [
-        {
-          title: 'Web',
-          cols: 2,
-          rows: 1,
-          src: '/assets/img/icons-web.png',
-        },
-        {
-          title: 'Sistemas',
-          cols: 2,
-          rows: 1,
-          src: '/assets/img/icons-sistemas.png',
-        },
-        {
-          title: 'Cloud',
-          cols: 2,
-          rows: 1,
-          src: '/assets/img/icons-cloud.png',
-        },
-        {
-          title: 'Consultoria',
-          cols: 2,
-          rows: 1,
-          src: '/assets/img/icons-consultoria.png',
-        },
-        {
-          title: 'Contacto',
-          cols: 2,
-          rows: 1,
-          src: '/assets/img/icons-contact.png',
-        },
-        {
-          title: 'Clientes',
-          cols: 2,
-          rows: 1,
-          src: '/assets/img/icons-clientes.png',
-        },
-      ];
-    })
-  );
-
-  constructor(private breakpointObserver: BreakpointObserver) {}
+  toggleMenu() {
+    this.isCollapsed = !this.isCollapsed;
+  }
 }
